0abef527416dac40223fe9aee8aba379ce8f9a78
Thuong Dang <61779420+thuongdang-logigear@users.noreply.github.com>
2024-02-02 15:57:55 +0700

    Update settings

---
+++ cypress.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "cypress";
+import { configureAllureAdapterPlugins } from '@mmisty/cypress-allure-adapter/plugins';
+import _ from "lodash/fp";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      configureAllureAdapterPlugins(on, config);
+
+      const environmentName = config.env.configFile || 'qat'
+      const environmentFilename = `./config/${environmentName}.json`
+      const envConfig = require(environmentFilename);
+
+      return _.merge(config, envConfig);
+    },
+    env: {
+      allure: true,
+      allureResults: "reports/allure-results",
+    }
+  },
+  watchForFileChanges: false,
+  screenshotsFolder: "reports/screenshots",
+  video: false,
+  videosFolder: "reports/videos",
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'reports/results',
+    overwrite: false,
+    html: true,
+    json: false,
+  },
+});
---
+++ cypress/e2e/login/DA_LOGIN_TC001.cy.ts
@@ -0,0 +1,3 @@
+it("Verify that user can login specific repository successfully via Dashboard login page with correct credentials", () => {
+  cy.visit("/TADashboard/login.jsp");
+});
